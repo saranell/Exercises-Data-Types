614d8d792258c7d5f7315bcae01ec173848aec26
Sara Nelson <76405224+saranell@users.noreply.github.com>
2022-08-05 16:53:16 -0500

    input/output, numbers, strings

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Strings
+string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+Console.WriteLine(alice);
+Console.WriteLine("Search for a word within this string: ");
+string searchWord = Console.ReadLine();
+string word = searchWord.ToLower();
+string aliceLower = alice.ToLower();
+
+if (aliceLower.IndexOf(word, 0) != -1)
+{
+    Console.WriteLine("true");
+    Console.WriteLine(alice.Length)
+
+}
+else
+{
+    Console.WriteLine("false");
+}
